61995fcca8ecd0f17f70ccce8ccaf3830c45cebb
Newtwork-Error <77990528+Newtwork-Error@users.noreply.github.com>
2021-05-10 17:31:36 -0500

    Create matrixNorm.cu

---
+++ matrixNorm.cu
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include <math.h>
+
+__global__ void cuda_hello(){
+  printf("Hello World from GPU!\n");
+}
+
+int main(){
+  cuda_hello<<<1,1>>>();
+  printf("Hello World from CPU!\n");
+  cudaGetLastError();
+  return 0;
+}
